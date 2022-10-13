fab0c4698fa468e9d403cd63e7948cf22f235785
EdgarMTdev <edgarmontiel961@gmail.com>
2022-10-13 10:16:31 -0500

    add: first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import PeopleComponent from './people/people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/people/people.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-people",
+    templateUrl: "./people.component.html",
+})
+class PeopleComponent {
+    name =  "Edgar"
+}
+
+export default PeopleComponent
