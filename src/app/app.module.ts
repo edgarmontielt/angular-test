5d2103f2849914628af9d2a87a1796950c6e05ec
EdgarMTdev <edgarmontiel961@gmail.com>
2022-10-13 10:29:36 -0500

    add: components in line and styles with tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import PeopleComponent from './people/people.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent
+    PeopleComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-people",
-    templateUrl: "./people.component.html",
+    // templateUrl: "./people.component.html",
+    template:`<div>
+        <h4>Name: {{ name }}</h4>
+        <app-person></app-person>
+        <app-person></app-person>
+    </div>`
 })
 class PeopleComponent {
     name =  "Edgar"
