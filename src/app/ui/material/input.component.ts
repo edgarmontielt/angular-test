5c7ab26e396c6e92fa0a6b86e46a5ad9a6b42e82
EdgarMTdev <edgarmontiel961@gmail.com>
2022-10-20 18:30:49 -0500

    add: filtered options in autocomplete

--- src/app/ui/material/input.component.ts
+++ src/app/ui/material/input.component.ts
@@ -1,14 +1,39 @@
-import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms"
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import data from '../../../data/products.json'
 
 @Component({
-    selector: "app-input-component",
-    templateUrl: "./input.component.html",
-    styleUrls: ["./input.component.css"]
+  selector: 'app-input-component',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
 })
-class InputComponent{
-    myControl = new FormControl();
-    options: string[] = ['One', 'Two', "Three"]
+class InputComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
+  products: object = data
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+    console.log(data)
+  }
+
+  private _filter(value: string): string[] {
+    const filteredValue = value.toUpperCase();
+
+    return this.options.filter((option) =>
+      option.toUpperCase().includes(filteredValue)
+    );
+  }
+
+  log(): void{
+    console.log("Hello");
+  }
 }
 
-export default InputComponent
+export default InputComponent;
